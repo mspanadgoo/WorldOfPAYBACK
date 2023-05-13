7810d2c12e7e912225b511ba31db37d4a0ea3f5f
mspanadgoo <mspanadgoo@me.com>
2023-05-13 08:25:03 +0330

    First Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Mohammad Sadegh Panadgoo on 2/23/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
